fbc39be4e1ab58082fe160c8a0487c37f78bb5d1
Maleyka Heybatova <6nisan2005@gmail.com>
2026-06-09 14:32:29 +0400

    add database package with DBConn export

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 import(
+	"github.com/prensmatt/go-fiber-crm-basic/database"
 	"github.com/gofiber/fiber/v2"
+	"fmt"
 )
 
 func setupRoutes(app *fiber.App){
@@ -10,8 +12,21 @@ func setupRoutes(app *fiber.App){
 	app.Delete("/leads/:id",DeleteLead)
 }
 
+func initDatabase(){
+	var err error
+	database.DBConn, err = gorm.Open("sqlite3","leads.db")
+	if err != nil{
+		panic("failed to connect database")
+	}
+	fmt.Println("Connection opened to database")
+	database.DBConn.AutoMigrate(&lead.Lead{})
+	fmt.Println("Database Migrated")
+}
+
 func main(){
 	app := fiber.New()
+	initDatabase()
 	setupRoutes(app)
 	app.Listen(":3000")
+	defer database.DBConn.Close()
 }
