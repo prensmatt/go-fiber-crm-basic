c8e935bb467aa08be53e59fa0ab5ea926bddb90a
Maleyka Heybatova <6nisan2005@gmail.com>
2026-06-09 15:03:23 +0400

    add lead model and GetLead handler

--- lead/lead.go
+++ lead/lead.go
@@ -1,6 +1,8 @@
 package lead
 import(
+	"github.com/prensmatt/go-fiber-crm-basic/database"
 	"github.com/jinzhu/gorm"
+	"github.com/gofiber/fiber/v2"
 )
 
 type Lead struct{
@@ -9,4 +11,24 @@ type Lead struct{
 	Company 	string
 	Email 		string
 	Phone 		int
+}
+
+func GetLeads(c *fiber.Ctx){
+
+}
+
+func GetLead(c *fiber.Ctx){
+	id := c.Params("id")
+	db := database.DBConn
+	var lead Lead
+	db.Find(&lead, id)
+	c.JSON(lead)
+}
+
+func NewLead(c *fiber.Ctx){
+
+}
+
+func DeleteLead(c *fiber.Ctx){
+
 }
